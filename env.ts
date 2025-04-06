434cc7c49a00528b9dd6b2c4f3e711e2045b8811
Parrot-zzt <13822593+parrot-zzt@user.noreply.gitee.com>
2025-04-06 13:42:01 +0100

    Completed Fan out implementation - added emailing logic

---
+++ env.ts
@@ -0,0 +1,3 @@
+export const SES_REGION = 'eu-west-1';
+export const SES_EMAIL_FROM = 'zhongzitian00@gmail.com' ;
+export const SES_EMAIL_TO =  '20109120@mail.wit.ie';
--- lib/eda-app-stack.ts
+++ lib/eda-app-stack.ts
@@ -28,6 +28,13 @@ export class EDAAppStack extends cdk.Stack {
         receiveMessageWaitTime: cdk.Duration.seconds(10),
       });
   
+
+      const mailerQ = new sqs.Queue(this, "mailer-queue", {
+        receiveMessageWaitTime: cdk.Duration.seconds(10),
+      });
+  
+
+
       const newImageTopic = new sns.Topic(this, "NewImageTopic", {
         displayName: "New Image topic",
       }); 
@@ -44,6 +51,16 @@ export class EDAAppStack extends cdk.Stack {
       memorySize: 128,
     }
   );
+  
+  const mailerFn = new lambdanode.NodejsFunction(this, "mailer-function", {
+    runtime: lambda.Runtime.NODEJS_16_X,
+    memorySize: 1024,
+    timeout: cdk.Duration.seconds(3),
+    entry: `${__dirname}/../lambdas/mailer.ts`,
+  });
+
+
+
 
   // S3 --> SQS
   imagesBucket.addEventNotification(
@@ -51,11 +68,7 @@ export class EDAAppStack extends cdk.Stack {
     new s3n.SnsDestination(newImageTopic)
   );
 
-  imagesBucket.addEventNotification(
-    s3.EventType.OBJECT_CREATED,
-    new s3n.SnsDestination(newImageTopic)  // Changed
-);
-
+    newImageTopic.addSubscription(new subs.SqsSubscription(mailerQ));
 
 
  // SQS --> Lambda
@@ -63,6 +76,27 @@ export class EDAAppStack extends cdk.Stack {
     batchSize: 5,
     maxBatchingWindow: cdk.Duration.seconds(5),
   });
+
+  const newImageMailEventSource = new events.SqsEventSource(mailerQ, {
+    batchSize: 5,
+    maxBatchingWindow: cdk.Duration.seconds(5),
+  }); 
+
+  mailerFn.addEventSource(newImageMailEventSource);
+
+
+  mailerFn.addToRolePolicy(
+    new iam.PolicyStatement({
+      effect: iam.Effect.ALLOW,
+      actions: [
+        "ses:SendEmail",
+        "ses:SendRawEmail",
+        "ses:SendTemplatedEmail",
+      ],
+      resources: ["*"],
+    })
+  );
+
 
   processImageFn.addEventSource(newImageEventSource);
 
